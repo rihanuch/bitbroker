05c42aeea6f9ee9ce0e22b9760dc554c6ddb53f3
rihanuch <rihanuch@uc.cl>
2020-08-13 17:22:52 -0400

    add bot route

--- bitbroker/urls.py
+++ bitbroker/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'bot/',  include('bot.urls')),
     re_path(r'api/',  include('api.urls')),
 ]
